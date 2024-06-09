2f45c28de6066d1b68120388fd17ffd415ef5de6
bbyork <massoda.thcoussi@gmail.com>
2024-06-10 01:43:38 +0200

    2.8: The Phonebook Step 3

--- part_1/src/App.jsx
+++ part_1/src/App.jsx
@@ -88,12 +88,15 @@ function areTheseObjectsEqual(first, second) {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: 12345 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, number: newNumber };
     const alreadyExist = persons.some((person) =>
       areTheseObjectsEqual(person, newPerson)
     );
@@ -101,11 +104,16 @@ const App = () => {
     else {
       setPersons(persons.concat(newPerson));
       setNewName("");
+      setNewNumber("");
     }
   };
 
-  const handleChange = (event) => {
+  const handleChangeName = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleChangeNumber = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -113,14 +121,16 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleChangeName} />
+          <br />
+          number: <input value={newNumber} onChange={handleChangeNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
+      <ul style={{ listStyleType: "none" }}>
         {persons.map((p) => (
           <Person key={p.name} person={p} />
         ))}
